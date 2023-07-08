af0d1646b826ddbdced507205dbd4a742525e2fa
Krisan-2024-ME <138972393+Krisan-2024-ME@users.noreply.github.com>
2023-07-08 14:52:21 +0530

    Add files via upload

---
+++ area-circle.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : area-circle.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	float pi = 3.14;
+	int radius = 4;
+	float area = pi*radius*radius;
+	printf("Are of cirle is %.2f square unit." ,area);
+	return 0;
+}
